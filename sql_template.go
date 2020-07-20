595c9ee6c83150a33bb69d5ba12e2d594a5fbeef
zlyuancn <zlyuancn@163.com>
2020-07-20 21:18:06 +0800

    优化sql模板解析

--- sql_template.go
+++ sql_template.go
@@ -11,6 +11,7 @@ package zstr
 import (
 	"fmt"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -19,51 +20,64 @@ const defaultSqlCompareFlag = "="
 var sqlTemplateRegex = regexp.MustCompile(`[&|@]\w+`)
 var sqlTemplateRegexCrust = regexp.MustCompile(`{[&|@]\w+}`)
 var sqlTemplateRegexCrustAndFlag = regexp.MustCompile(`{[&|]\w+ .+?}`)
-var sqlTemplateParseNameRegex = regexp.MustCompile(`{{\w+}}`)
+var sqlTemplateParseNameRegex = regexp.MustCompile(`{{\d+}}`)
 
 type sqlTemplate struct {
-	data map[string]interface{}
+	data   map[string]interface{}
+	index  uint64
+	names  []string
+	values []interface{}
 }
 
-func (m *sqlTemplate) Parse(sql_template string, kvs ...interface{}) (sql_str string, names []string, args []interface{}) {
-	data := makeMapOfkvs(kvs)
-	m.data = make(map[string]interface{})
+func newSqlTemplate(kvs ...interface{}) *sqlTemplate {
+	return &sqlTemplate{
+		data: makeMapOfkvs(kvs),
+	}
+}
 
+func (m *sqlTemplate) addValue(name string, value interface{}) (flag string) {
+	flag = "{{" + strconv.FormatUint(m.index, 10) + "}}"
+	m.names = append(m.names, name)
+	m.values = append(m.values, value)
+	m.index++
+	return
+}
+
+func (m *sqlTemplate) Parse(sql_template string) (sql_str string, names []string, args []interface{}) {
 	sql_str = sqlTemplateRegexCrust.ReplaceAllStringFunc(sql_template, func(s string) string {
-		return m.translate(s[1:len(s)-1], defaultSqlCompareFlag, true, data)
+		return m.translate(s[1:len(s)-1], defaultSqlCompareFlag, true)
 	})
 
 	sql_str = sqlTemplateRegexCrustAndFlag.ReplaceAllStringFunc(sql_str, func(s string) string {
 		k := strings.Index(s, " ")
-		return m.translate(s[1:k], s[k+1:len(s)-1], true, data)
+		return m.translate(s[1:k], s[k+1:len(s)-1], true)
 	})
 
 	sql_str = sqlTemplateRegex.ReplaceAllStringFunc(sql_str, func(s string) string {
-		return m.translate(s, defaultSqlCompareFlag, false, data)
+		return m.translate(s, defaultSqlCompareFlag, false)
 	})
 
+	// 按顺序写入names和args
 	sql_str = sqlTemplateParseNameRegex.ReplaceAllStringFunc(sql_str, func(s string) string {
-		name := s[2 : len(s)-2]
-		names = append(names, name)
-		args = append(args, m.data[name])
+		index, _ := strconv.Atoi(s[2 : len(s)-2])
+		names = append(names, m.names[index])
+		args = append(args, m.values[index])
 		return "?"
 	})
 
 	sql_str = repairSql(sql_str)
-
 	return sql_str, names, args
 }
 
-func (m *sqlTemplate) translate(text, flag string, crust bool, data map[string]interface{}) string {
+func (m *sqlTemplate) translate(text, flag string, crust bool) string {
 	operation, name, cflag := text[:1], text[1:], strings.ToLower(flag)
 
-	value, has := data[name]
+	value, has := m.data[name]
 
 	switch operation {
 	case "@":
 		if has {
-			m.data[name] = value
-			return "{{" + name + "}}"
+			return m.addValue(name, value)
 		}
 		if crust {
 			return "null"
@@ -77,21 +91,31 @@ func (m *sqlTemplate) translate(text, flag string, crust bool, data map[string]i
 		panic(fmt.Errorf(`syntax error, non-supported operation "%s"`, operation))
 	}
 
-	var sql_str string
+	var makeSqlStr func() string
 	switch cflag {
 	case ">", ">=", "<", "<=", "!=", "<>", "=":
-		sql_str = fmt.Sprintf(`%s %s %s {{%s}}`, operation, name, cflag, name)
+		makeSqlStr = func() string {
+			return fmt.Sprintf(`%s %s %s %s`, operation, name, cflag, m.addValue(name, value))
+		}
 	case "in", "not in":
-		sql_str = fmt.Sprintf(`%s %s %s ({{%s}})`, operation, name, cflag, name)
+		makeSqlStr = func() string {
+			return fmt.Sprintf(`%s %s %s (%s)`, operation, name, cflag, m.addValue(name, value))
+		}
 	case "like": // 包含xx
-		sql_str = fmt.Sprintf(`%s %s like {{%s}}`, operation, name, name)
-		value = "%" + anyToSqlString(value, false) + "%"
+		makeSqlStr = func() string {
+			value = "%" + anyToSqlString(value, false) + "%"
+			return fmt.Sprintf(`%s %s like %s`, operation, name, m.addValue(name, value))
+		}
 	case "likestart", "like_start": // 以xx开始
-		sql_str = fmt.Sprintf(`%s %s like {{%s}}`, operation, name, name)
-		value = anyToSqlString(value, false) + "%"
+		makeSqlStr = func() string {
+			value = anyToSqlString(value, false) + "%"
+			return fmt.Sprintf(`%s %s like %s`, operation, name, m.addValue(name, value))
+		}
 	case "likeend", "like_end": // 以xx结束
-		sql_str = fmt.Sprintf(`%s %s like {{%s}}`, operation, name, name)
-		value = "%" + anyToSqlString(value, false)
+		makeSqlStr = func() string {
+			value = "%" + anyToSqlString(value, false)
+			return fmt.Sprintf(`%s %s like %s`, operation, name, m.addValue(name, value))
+		}
 	default:
 		panic(fmt.Errorf(`syntax error, non-supported flag "%s"`, flag))
 	}
@@ -102,13 +126,12 @@ func (m *sqlTemplate) translate(text, flag string, crust bool, data map[string]i
 	if value == nil {
 		return fmt.Sprintf(`%s %s is null`, operation, name)
 	}
-	m.data[name] = value
-	return sql_str
+	return makeSqlStr()
 }
 
 // sql模板解析, 同 SqlTemplateRender, 但是它返回的是orm使用的sql语句和参数
 func SqlTemplateParse(sql_template string, kvs ...interface{}) (sql_str string, names []string, args []interface{}) {
-	return new(sqlTemplate).Parse(sql_template, kvs...)
+	return newSqlTemplate(kvs...).Parse(sql_template)
 }
 
 // sql模板渲染, 注意, 这个函数不支持sql注入检查
