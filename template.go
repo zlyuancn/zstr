9cca696f850e84fc2fcf8bc8d597d0e9f20c9804
zlyuancn <zlyuancn@163.com>
2020-11-25 14:00:36 +0800

    合并模板正则

--- template.go
+++ template.go
@@ -12,8 +12,7 @@ import (
 	"regexp"
 )
 
-var templateRegex = regexp.MustCompile(`@\w*\.?\w+`)
-var templateRegexCrust = regexp.MustCompile(`\{@\w*\.?\w+\}`)
+var templateRegex = regexp.MustCompile(`(\{@\w*\.?\w+\})|(@\w*\.?\w+)`)
 
 // 模板渲染
 //
@@ -24,17 +23,16 @@ var templateRegexCrust = regexp.MustCompile(`\{@\w*\.?\w+\}`)
 //    s:=TemplateRender("s{@a}e", map[string]string{"a": "v"})
 func TemplateRender(format string, kvs ...interface{}) string {
 	data := makeMapOfkvs(kvs)
-	// 替换 {@field}, 如果没有设置则替换为空字符串
-	result := templateRegexCrust.ReplaceAllStringFunc(format, func(s string) string {
-		v, ok := data[s[2:len(s)-1]]
-		if ok {
-			return anyToString(v)
+	// 替换 {@field} 和 @field, 如果没有设置则不替换
+	result := templateRegex.ReplaceAllStringFunc(format, func(s string) string {
+		var key string
+		if s[0] == '{' {
+			key = s[2 : len(s)-1]
+		} else {
+			key = s[1:]
 		}
-		return ""
-	})
-	// 替换 @field, 如果没有设置则不替换
-	result = templateRegex.ReplaceAllStringFunc(result, func(s string) string {
-		v, ok := data[s[1:]]
+
+		v, ok := data[key]
 		if ok {
 			return anyToString(v)
 		}
