18744205a021a9cc32d25823d15fab75cfdfcfc1
zlyuancn <zlyuancn@163.com>
2020-11-25 20:47:01 +0800

    修复合并template正则时未将未设置值的带外壳的变量替换为空字符串

--- template.go
+++ template.go
@@ -29,8 +29,10 @@ func TemplateRender(format string, kvs ...interface{}) string {
 	// 替换 {@field} 和 @field, 如果没有设置则不替换
 	result := templateRegex.ReplaceAllStringFunc(format, func(s string) string {
 		var key string
+		var crust bool
 		if s[0] == '{' {
 			key = s[2 : len(s)-1]
+			crust = true
 		} else {
 			key = s[1:]
 		}
@@ -41,6 +43,9 @@ func TemplateRender(format string, kvs ...interface{}) string {
 		}
 		if ok {
 			return anyToString(v)
+		}
+		if crust {
+			return ""
 		}
 		return s
 	})
