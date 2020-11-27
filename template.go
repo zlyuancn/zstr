426bfe1142e4d48e4175ebc355853152b9781bd1
zlyuancn <zlyuancn@163.com>
2020-11-27 21:03:27 +0800

    template相关移动到*simpleTemplate内

--- template.go
+++ template.go
@@ -15,17 +15,19 @@ import (
 
 var templateRegex = regexp.MustCompile(`(\{@\w*\.?\w+\})|(@\w*\.?\w+)`)
 
-// 模板渲染
-//
-// 输入的kvs必须为：map[string]string，map[string]interface{}，或健值对
-// 示例:
-//    s:=TemplateRender("s@a e", "a", "v")
-//    s:=TemplateRender("s{@a}e", "a", "v")
-//    s:=TemplateRender("s{@a}e", map[string]string{"a": "v"})
-//    s:=TemplateRender("s@a @a e", "a", "v", "a[1]", "xxx")
-func TemplateRender(format string, kvs ...interface{}) string {
-	data := makeMapOfkvs(kvs)
-	c := newCounter()
+type simpleTemplate struct {
+	data    map[string]interface{}
+	counter counter
+}
+
+func newSimpleTemplate(kvs ...interface{}) *simpleTemplate {
+	return &simpleTemplate{
+		data:    makeMapOfkvs(kvs),
+		counter: newCounter(),
+	}
+}
+
+func (m *simpleTemplate) Render(format string) string {
 	// 替换 {@field} 和 @field, 如果没有设置则不替换
 	result := templateRegex.ReplaceAllStringFunc(format, func(s string) string {
 		var key string
@@ -37,9 +39,9 @@ func TemplateRender(format string, kvs ...interface{}) string {
 			key = s[1:]
 		}
 
-		v, ok := data[key+"["+strconv.Itoa(c.Incr(key)-1)+"]"]
+		v, ok := m.data[key+"["+strconv.Itoa(m.counter.Incr(key)-1)+"]"]
 		if !ok {
-			v, ok = data[key]
+			v, ok = m.data[key]
 		}
 		if ok {
 			return anyToString(v)
@@ -52,7 +54,19 @@ func TemplateRender(format string, kvs ...interface{}) string {
 	return result
 }
 
+// 模板渲染
+//
+// 输入的kvs必须为：map[string]string，map[string]interface{}，或健值对
+// 示例:
+//    s:=TemplateRender("s@a e", "a", "v")
+//    s:=TemplateRender("s{@a}e", "a", "v")
+//    s:=TemplateRender("s{@a}e", map[string]string{"a": "v"})
+//    s:=TemplateRender("s@a @a e", "a", "v", "a[1]", "xxx")
+func TemplateRender(format string, kvs ...interface{}) string {
+	return newSimpleTemplate(kvs...).Render(format)
+}
+
 // 模板渲染, 和TemplateRender一样, 只是简短了函数名
 func Render(format string, kvs ...interface{}) string {
-	return TemplateRender(format, kvs...)
+	return newSimpleTemplate(kvs...).Render(format)
 }
