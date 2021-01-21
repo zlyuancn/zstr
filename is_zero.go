daa622b5ac440093d4a9e07a69939084ed487113
zlyuancn <zlyuancn@163.com>
2021-01-21 13:02:44 +0800

    居然还有interface{}(0)不等于0的!!!

--- is_zero.go
+++ is_zero.go
@@ -25,9 +25,36 @@ func IsZero(a interface{}) bool {
 	case bool:
 		return !v
 
-	case int, int8, int16, int32, int64,
-		uint, uint8, uint16, uint32, uint64, uintptr,
-		float32, float64, complex64, complex128:
+	case int:
+		return v == 0
+	case int8:
+		return v == 0
+	case int16:
+		return v == 0
+	case int32:
+		return v == 0
+	case int64:
+		return v == 0
+
+	case uint:
+		return v == 0
+	case uint8:
+		return v == 0
+	case uint16:
+		return v == 0
+	case uint32:
+		return v == 0
+	case uint64:
+		return v == 0
+
+	case float32:
+		return v == 0
+	case float64:
+		return v == 0
+
+	case complex64:
+		return v == 0
+	case complex128:
 		return v == 0
 	}
 
