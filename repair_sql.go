10dd23066b743bc1d06014a54fc87e381be7ed74
zlyuancn <zlyuancn@163.com>
2021-01-22 16:30:09 +0800

    修复searchIgnoreCase偶然情况下会panic

--- repair_sql.go
+++ repair_sql.go
@@ -153,7 +153,7 @@ func (m *sqlTemplate) searchIgnoreCase(ss []rune, sub []rune, start int) int {
 			break
 		}
 	}
-	if !has {
+	if !has || len(ss)-start < len(sub) {
 		return -1
 	}
 	for i := 1; i < len(sub); i++ {
